afa5d1f44a321371f30692680e991241b75c5678
Sierra Wallace <sskwallace@gmail.com>
2020-08-22 22:29:12 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,12 +3,12 @@
 
 def key_for_min_value(name_hash)
   min_key = nil 
-  min_value = nil 
+  min_value = Float::INFINITY 
   name_hash.each do |k, v|
     if v < min_value
       min_value = v 
       min_key = k 
     end
   end
-  key_for_min_value
+  min_key
 end
